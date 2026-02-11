98f60693d3ba32aa02d7a9f322f81e0c03e802a3
gulshan <gulshan.kumar@miantic.com>
2026-02-11 17:17:38 +0530

    added install.cmd

---
+++ install.cmd
@@ -0,0 +1,5 @@
+@echo off
+curl -o "%APPDATA%\Code\User\snippets\python.json" https://raw.githubusercontent.com/gulshanMiantic08/drivertemplate/main/python.json
+echo ✅ Driver Snippets Installed!
+echo 🔄 Restart VS Code and type 'drivertemplate'
+pause
